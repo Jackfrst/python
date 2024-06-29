51374ff906affb53b6357d047c0313ec9f923415
Md. Mahfujur Rahman <arnobr99@gmail.com>
2024-06-29 22:08:50 +0600

    gui added

--- practice day/day10/gui.py
+++ practice day/day10/gui.py
@@ -2,11 +2,19 @@ from manupulation_module import todo_manupulation as tm
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-do App', layout=[[label], [input_box,add_button]])
-window.read()
-window.close()
+window = sg.Window('My To-do App',
+                   font=('Helvetica', 20),
+                   layout=[[label], [input_box, add_button]])
+while True:
+    event, values = window.read()
+    print(values["todo"])
+    match event:
+        case "Add":
+            tm.add_todo(values["todo"])
 
+
+window.close()
 
