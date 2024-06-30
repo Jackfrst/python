efec57cee4940580e29f92ea84c622103c0dd9be
Md. Mahfujur Rahman <arnobr99@gmail.com>
2024-06-30 17:02:08 +0600

    gui edit [30-6-24]

--- practice day/day10/gui.py
+++ practice day/day10/gui.py
@@ -1,13 +1,21 @@
 from manupulation_module import todo_manupulation as tm
+from manupulation_module import file_manupulation as fm
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
-
+list_box = sg.Listbox(values=fm.get_todo("files/todos.txt"),
+                      key='todos',
+                      enable_events=True,
+                      size=(45, 10))
+edit_button = sg.Button("Edit")
 window = sg.Window('My To-do App',
                    font=('Helvetica', 20),
-                   layout=[[label], [input_box, add_button]])
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button]])
+
 while True:
     event, values = window.read()
     print(values["todo"])
